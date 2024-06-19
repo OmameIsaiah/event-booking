a9cdc8c8a1647d97ebdc2b2d10025ca535e17753
Isaiah Omame <isaiah@swipe.ng>
2024-06-19 04:40:24 +0100

    Added security restrictions to ADMIN endpoings

--- src/main/java/com/event/booking/security/jwt/WebSecurityConfig.java
+++ src/main/java/com/event/booking/security/jwt/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.event.booking.security.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -75,6 +76,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/users/management/filter").hasAnyRole("CAN_VIEW_USERS", "CAN_DELETE_USERS")
                 .antMatchers("/api/v1/users/management/search").hasAnyRole("CAN_VIEW_USERS", "CAN_DELETE_USERS")
                 .antMatchers("/api/v1/users/management/delete").hasRole("CAN_DELETE_USERS")
+                .antMatchers(HttpMethod.POST, "/api/v1/events").hasAnyRole("CAN_CREATE_EVENT", "CAN_UPDATE_EVENT", "CAN_DELETE_EVENT")
+                .antMatchers(HttpMethod.PUT, "/api/v1/events").hasAnyRole("CAN_CREATE_EVENT", "CAN_UPDATE_EVENT", "CAN_DELETE_EVENT")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/events/{eventId}").hasAnyRole("CAN_CREATE_EVENT", "CAN_UPDATE_EVENT", "CAN_DELETE_EVENT")
+                .antMatchers(HttpMethod.GET, "/api/v1/events/all/reservations").hasAnyRole("CAN_CREATE_EVENT", "CAN_UPDATE_EVENT", "CAN_DELETE_EVENT")
+                .antMatchers(HttpMethod.GET, "/api/v1/events//all/reservations/filter").hasAnyRole("CAN_CREATE_EVENT", "CAN_VIEW_ALL_RESERVATIONS")
                 .anyRequest()
                 .authenticated()
                 .and()
